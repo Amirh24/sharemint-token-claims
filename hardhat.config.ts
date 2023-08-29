fe2bc7e03ec132a53eff8b6d2c04d4c0576e3c15
Eliezer Steinbock <eliesteinbock@gmail.com>
2023-08-29 13:18:52 +0300

    add deploy sepolia script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
+require('dotenv').config()
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY!]
+    }
+  }
 };
 
 export default config;
